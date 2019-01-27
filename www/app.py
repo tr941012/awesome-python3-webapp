099275562638aaf98f8fe3b8f705f0c89be6e412
mucci <tianruimucci@163.com>
2019-01-27 16:44:22 +0800

    Day1:Webapp框架，可以在页面上展示内容
    Signed-off-by: mucci <tianruimucci@163.com>

---
+++ www/app.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/env python3.7
+# -*- coding: utf-8 -*-
+# @Time : 2019-01-27 15:55
+# @Author : 二大爷
+# @Site :
+# @File : app.py
+# @Software: PyCharm
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio, os, json, time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1> <h2>Shitty</h2>',content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
